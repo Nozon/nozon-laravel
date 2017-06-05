41768f41fbc215ef595c7312512fee5e65e22d74
Nicolas M <nicolas.mermoud@heig-vd.ch>
2017-06-05 12:31:02 +0200

    Ajout test.php
    Ajout d'un fichier de test

---
+++ public/test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Un fichier de test pour s'entraîner avec Github
+ * User: Nicolas
+ * Date: 05.06.2017
+ * Time: 11:59
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+      <title>Une page de test</title>
+  </head>
+  <body>
+  <p>Une page de test</p>
+  </body>
+</html>
