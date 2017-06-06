a4b82834aef019e5313576755d462434da38811f
eulerien <aurelienviret@mbp-de-aurelien.einet.ad.eivd.ch>
2017-06-06 13:12:27 +0200

    Création de toutes les migrations
    Il devrait y avoir 21 tables

---
+++ database/migrations/2017_06_06_103716_create_recompenses_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecompensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recompenses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recompenses');
+    }
+}
