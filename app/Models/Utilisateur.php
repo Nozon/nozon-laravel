5ae906524ed2ece8b13512d665ded1d7bfee6deb
swisspix <antoinum@gmail.com>
2017-06-08 23:31:53 +0200

    Correction des uses

--- app/Models/Utilisateur.php
+++ app/Models/Utilisateur.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Models\App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\Models\User as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Utilisateur extends Authenticatable
 {
