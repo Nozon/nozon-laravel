3c7cdbcf9a624865ae8af277d25cab1a0eea5ddf
Nicolas M <nicolas.mermoud@heig-vd.ch>
2017-06-13 19:35:37 +0200

    Tests liens front/back avec les articles de presse

--- app/Models/Presse.php
+++ app/Models/Presse.php
@@ -10,30 +10,32 @@ use Illuminate\Support\Facades\Validator;
 
 class Presse extends Model
 {
-
     public static $rules = [
         'url' => ['required', 'string'],
         'titre' => ['required', 'string'],
         'description' => ['required', 'string'],
-
-        'date' => ['required', 'date']
+        'date' => ['required', 'string'],
+        'edition_annee' => ['sometimes', 'required', 'integer']
     ];
+
+    protected $table = 'presses';
 
     public static function getValidation(Request $request)
     {
-        // Récupération des inputs
-        $inputs = $request->only('url', 'titre', 'description', 'date');
-
+        echo("Dans le validateur <br />");
+        $inputs = $request->only('url', 'titre', 'description', 'date', 'edition_annee');
         // Création du validateur
         $validator = Validator::make($inputs, Presse::$rules);
         // Ajout des contraintes supplémentaires
         $validator->after(function ($validator) use ($inputs) {
             // Vérification de la non-existence du Presse
             if (Presse::exists($inputs['url'])) {
+                echo("Erreur validation : presse existe déjà <br />");
                 $validator->errors()->add('exists', Message::get('presse.exists'));
             }
         });
         // Renvoi du validateur
+        echo("Validation terminée <br />");
         return $validator;
     }
 
@@ -49,19 +51,18 @@ class Presse extends Model
      * @param array $values
      */
     public static function createOne(array $values) {
-
         // Création d'une nouvelle instance de Presse
         $new = new Presse();
-
         // Définition des propriétés de Presse
         $new->url = $values['url'];
         $new->titre = $values['titre'];
         $new->description = $values['description'];
         $new->date = $values['date'];
-      
+        $new->edition_annee = $values['edition_annee'];
         // Enregistrement de Presse
         $new->save();
     }
+
 
      public function edition(){
         return $this->belongsTo('App/Models/Edition');
--- resources/views/pages/presse/create.blade.php
+++ resources/views/pages/presse/create.blade.php
@@ -2,24 +2,30 @@
                 <div class="row">
                     <div class="administration" id="creerEdition">
                         <button type="button" id="btn-show-form" class="btn btn-default btn-lg btn-add-press">Ajouter un article de presse</button>
-                        <form class="form-add-press">
+                        <form class="form-add-press" method="POST" action="{{ url('presse') }}">
                             <div class="form-group">
                                 <label>Nom du journal</label>
-                                <input type="text" class="form-control"/>
+                                <input type="text" class="form-control" placeholder="titre" name="titre" value="{{ old('titre') }}" />
                             </div>
                             
                             <div class="form-group">
                                 <label>Date de l'article</label>
-                                <input type="date" class="form-control"/>
+                                <input type="date" class="form-control" placeholder="date" name="date" value="{{ old('date') }}" />
                             </div>
                             <div class="form-group">
                                 <label>Lien de l'article</label>
-                                <input type="url" class="form-control"/>
+                                <input type="url" class="form-control" placeholder="url" name="url" value="{{ old('url') }}" />
                             </div>
                             <div class="form-group">
                                 <label>Texte</label>
-                                <textarea class="form-control" rows="3" placeholder="Ecrire ici..."></textarea>
+                                <textarea class="form-control" rows="3" placeholder="Ecrire ici..." name="description" value="{{ old('description') }}"></textarea>
                             </div>
+
+                            <div class="form-group">
+                                <label>Lien de l'article</label>
+                                <input type="text" placeholder="edition_annee" name="edition_annee" value="{{ old('edition_annee') }}" />
+                            </div>
+
                             <div class="form-group">
                                 <button class="btn btn-default btnAddPress">Ajouter</button>
                             </div>
