ea8580208adafefc741f04450798f629c255eb63
samivnt <samix1.6@gmail.com>
2017-06-13 09:25:00 +0200

    Ajout gallery img
    Ajut d'une gallerie d'img le 12.06

--- resources/views/pages/media/media.blade.php
+++ resources/views/pages/media/media.blade.php
@@ -1,30 +1,47 @@
 <!-- PARTIE MEDIA-->
-<div class="content-media-hydro">
-    <div class="row">
+<div class="content-media-hydro gallery-container">
         <div class="team-hydro-titre">
             <h1>Les médias</h1>
         </div>
-        <div class="grid media-description">
-            <h2>Photos</h2>
-            <div class="col">
-                <img src="img/facebooklogo.png"/>
-                <img src="img/facebooklogo.png"/>
-                <img src="img/facebooklogo.png"/>
-                <img src="img/facebooklogo.png"/>
-                <img src="img/facebooklogo.png"/>
-                <img src="img/facebooklogo.png"/>
-                <img src="img/facebooklogo.png"/>
-                <img src="img/facebooklogo.png"/>
-                <img src="img/facebooklogo.png"/>
-                <img src="img/facebooklogo.png"/>
+        <div class="tz-gallery">
+
+            <div class="row">
+
+                <div class="col-sm-12 col-md-4">
+                    <a class="lightbox" href="img/johnaesch.jpg">
+                        <img src="img/johnaesch.jpg" alt="Bridge">
+                    </a>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <a class="lightbox" href="img/trump.jpg">
+                        <img src="img/trump.jpg" alt="Park">
+                    </a>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <a class="lightbox" href="img/johnaesch.jpg">
+                        <img src="img/johnaesch.jpg" alt="Tunnel">
+                    </a>
+                </div>
+                <div class="col-sm-12 col-md-8">
+                    <a class="lightbox" href="img/trump.jpg">
+                        <img src="img/trump.jpg" alt="Traffic">
+                    </a>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <a class="lightbox" href="img/johnaesch.jpg">
+                        <img src="img/johnaesch.jpg" alt="Coast">
+                    </a>
+                </div> 
+                <div class="col-sm-6 col-md-4">
+                    <a class="lightbox" href="img/trump.jpg">
+                        <img src="img/trump.jpg" alt="Rails">
+                    </a>
+                </div>
+
             </div>
-            <h2>Vidéos</h2>
-            <div class="col">
-                <img src="img/sponsor.gif"/>
-                <img src="img/sponsor.gif"/>
-                <img src="img/sponsor.gif"/>
-            </div>
+
         </div>
+
     </div>
 </div>
 <!-- FIN PARTIE MEDIA--> 
