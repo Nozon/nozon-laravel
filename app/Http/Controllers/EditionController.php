415d0a45671f629c89af5bb7fd80b1c586e9848a
Nicolas M <nicolas.mermoud@heig-vd.ch>
2017-06-15 13:42:28 +0200

    EditionController en cours de modification

--- app/Http/Controllers/EditionController.php
+++ app/Http/Controllers/EditionController.php
@@ -10,9 +10,13 @@ use Illuminate\Support\Facades\DB;
 
 use App\Models\Edition;
 use App\Models\Equipe;
+use App\Models\Media;
 use App\Models\Membre;
 use App\Models\Presse;
+use App\Models\Profil;
+use App\Models\Publication;
 use App\Models\Recompense;
+use App\Models\Sponsor;
 
 use Illuminate\Http\Request;
 
@@ -28,54 +32,100 @@ class EditionController extends Controller {
         // Liste de toutes les éditions pour création dynamique du menu
         $editions = DB::table('editions')->orderBy('annee','desc')->get();
 
-//        echo "Listes des éditions : ";
-//        foreach($editions as $edition) {
-//            echo $edition->annee . " ";
-//        }
-//        echo "<br />";
+        // Récupération des informations de l'édition
+        $edition = Edition::where('annee', $annee)->first();
 
         // Récupération de l'id de l'équipe principale de l'édition concernée
         $equipePrincipale = Equipe::where('edition_annee', $annee)->where('type', 'principal')->first();
 
-//        echo("Equipe principale : " . $equipePrincipale->id. "<br />");
-
         // Récupération de l'id de l'équipe secondaire de l'édition concernée
         $equipeSecondaire = Equipe::where('edition_annee', $annee)->where('type', 'secondaire')->first();
 
-//        echo("Equipe secondaire : " . $equipeSecondaire->id. "<br />");
 
-        // Récupération des membres
-        $membresEqPrinc = Equipe::where('id', $equipePrincipale->id)->first();
+        // TESTS
 
-//        echo "Listes des membres : ";
-//        foreach($membresEqPrinc as $membreEqPrinc) {
-//            echo $membreEqPrinc->fonction . " ";
-//        }
-//        echo "<br />";
+        // On récupère un profil
+        // $profil = Profil::where('id', 1)->first();
+        // On l'affiche
+        // echo($profil);
+        // On récupère l'équipe d'après le profil
+        // echo($profil->equipe_id);
+        // echo(Equipe::where('id', $profil->equipe_id)->first());
+
+
+
+        // Récupération des profils des membres de l'équipe principale
+        $membresPrincipaux = Profil::all()->where('equipe_id', $equipePrincipale->id);
+
+        // Récupération des profils des membres de l'équipe secondaire
+        $membresSecondaires = Profil::all()->where('equipe_id', $equipeSecondaire->id);
+
+        // Récupération des informations du concours
+        $concours = $edition;
+
+        // Récupération des publications a.k.a. news
+        $news = Publication::all()->where('edition_annee', $annee);
+
+        // Récupération des medias
+        $medias = Media::all()->where('edition_annee', $annee);
 
         // Récupération des récompenses
         $recompenses = Recompense::all()->where('equipe_id', $equipePrincipale->id);
 
-//        echo "Listes des récompenses : ";
-//        foreach($recompenses as $recompense) {
-//            echo $recompense->type . " ";
-//        }
-//        echo "<br />";
-
         // Récupération des presses
         $presses = Presse::all()->where('edition_annee', $annee);
 
-//        echo "Listes des presses : ";
-//        foreach($presses as $presse) {
-//            echo $presse->titre . " ";
-//        }
-//        echo "<br />";
+        // Récupération des sponsors
+        $edition_sponsors = edition_sponsorController::all()->where('edition_annee', $annee);
+        $sponsorMain = Sponsor::where('valeur', 'principal')->first();
+
+        $sponsorMain = Sponsor::all()->where('edition_annee', $annee);
 
         return view('pages.edition')
             ->with('editions', $editions)
             ->with('equipePrincipale', $equipePrincipale)
+            ->with('equipeSecondaire', $equipePrincipale)
+            ->with('membresPrincipaux', $membresPrincipaux)
+            ->with('membresSecondaires', $membresSecondaires)
+            ->with('concours', $concours)
+            ->with('news', $news)
+            ->with('medias', $medias)
             ->with('presses', $presses)
+            ->with('sponsorMain', $sponsorMain)
             ->with('recompenses', $recompenses);
+
+        // TESTS
+        echo "Listes des éditions : ";
+        foreach($editions as $edition) {
+            echo $edition->annee . " ";
+        }
+
+        echo "<br />";
+        echo("Equipe principale : " . $equipePrincipale->id. "<br />");
+
+        echo("Equipe secondaire : " . $equipeSecondaire->id. "<br />");
+
+        echo "Listes des membres de l'équipe principale :<br />";
+        foreach($membresPrincipaux as $membreEqPrinc) {
+            echo $membreEqPrinc . "<br />";
+        }
+
+        echo "Listes des membres de l'équipe secondaire :<br />";
+        foreach($membresEqSec as $membreEqSec) {
+            echo $membreEqSec . "<br />";
+        }
+
+        echo "Listes des récompenses : ";
+        foreach($recompenses as $recompense) {
+            echo $recompense->type . " ";
+        }
+        echo "<br />";
+
+        echo "Listes des presses : ";
+        foreach($presses as $presse) {
+            echo $presse->titre . " ";
+        }
+        echo "<br />";
     }
 
     /**
