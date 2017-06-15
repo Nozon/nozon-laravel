72e3d7e293074f75b0675d2d6cb2b731237c0303
guidoAlreadyTaken <chris.baer9105@gmail.com>
2017-06-15 13:27:48 +0200

    13.27

---
+++ resources/views/pages/concours/edit.blade.php
@@ -0,0 +1,20 @@
+<h4><button type="button" id="year" class="btn btn-default year-edition">Modifier l'édition</button></h4>
+<form class="form-mod">
+    <div class="form-group">
+        <label>Année</label>
+        <input type="number" class="form-control" placeholder="Année">
+    </div>
+    <div class="form-group">
+        <label>Insérer une image du concours</label>
+        <input type="file" name="imgConcours"/>
+    </div>
+    <div class="form-group">
+        <label>Date du concours</label>
+        <input type="date" name="dateConcours" class="form-control"/>
+    </div>
+    <div class="form-group">
+        <label>Decription</label>
+        <textarea class="form-control" rows="3" placeholder="Ecrire ici..."></textarea>
+    </div>
+    <button class="btn btn-nozon" id="btnModifier">Modifier l'édition</button>
+</form>
